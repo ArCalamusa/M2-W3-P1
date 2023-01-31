d3aca6cb1fafdaa5b0a6c7a3b4cc1655b8738917
Arcangelo Calamusa <117526559+ArCalamusa@users.noreply.github.com>
2023-01-31 20:22:39 +0100

    Update M2-W3-P1-EXTRA.js

--- M2-W3-P1-EXTRA.js
+++ M2-W3-P1-EXTRA.js
@@ -84,27 +84,22 @@ const addToShoppingCart = function (itemToAdd) {
 }
 console.log(addToShoppingCart(newItem))
 
-//extra 4
-
-const maxShoppingCart = function (cart) {
-    let item = cart[0]
-    for (let i = 1; i < cart.length; i++) {
-        if (cart[i].price > item.price) {
-            item = cart[i]
-        }
-
-    }
-    return item
-}
-
-console.log(maxShoppingCart(shoppingCart));
-
 /* EXTRA 4
  Nel tuo sito e-commerce hai un array di oggetti chiamato shoppingCart. Ognuno di questi oggetti ha un prezzo, un nome, un id e la quantità da spedire.
  Crea una funzione chiamata "maxShoppingCart" che riceve l'array shoppingCart e ritorna l'oggetto più costoso in esso contenuto.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const maxShoppingCart = function (cart) {
+    let mostExpensiveItem = cart[0]
+    for (let i = 1; i < cart.length; i++) {
+        if (cart[i].price > mostExpensiveItem.price) {
+            mostExpensiveItem = cart[i]
+        }
+    }
+    return mostExpensiveItem
+}
+
+console.log(maxShoppingCart(shoppingCart))
 
 /* EXTRA 5
  Nel tuo sito e-commerce hai un array di oggetti chiamato shoppingCart. Ognuno di questi oggetti ha un prezzo, un nome, un id e la quantità da spedire.
@@ -124,40 +119,103 @@ const latestShoppingCart = function (m) {
  La funzione mostra in console un numero casuale tra 0 e 9 finchè il numero estratto è maggiore di x per 3 volte di fila.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const loopUntil = function (n) {
+    let counter = 0
+    while (counter !== 3) {
+        let random = Math.floor(Math.random() * 10)
+        console.log(random)
+        if (random > n) {
+            counter++
+        } else {
+            counter = 0
+        }
+    }
+}
+console.log(loopUntil(8));
 
 /* EXTRA 7
- Crea una funzione chiamata "average" che riceve un array come parametro e ritorna la media aritmetica dei numeri in esso contenuti.
- La funzione salta automaticamente qualsiasi valore non numerico all'interno dell'array.
-*/
+ Crea una funzione chiamata "average" che riceve un array come parametro e ritorna la media aritmetica dei numeri in esso contenuti. La funzione salta automaticamente qualsiasi valore non numerico all'interno dell'array.*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function average(arr) {
+    let somma = 0;
+    let count = 0;
+    for(let i = 0; i < arr.length; i++) {
+        if(typeof arr[i] === "number") {
+            somma += arr[i];
+            count++;
+        }
+    }
+    let media = somma / count;
+    return media;
+}
+console.log(average([2, 5, 8, "ciao", "wow", 9]));
 
 /* EXTRA 8
- Scrivi una funzione chiamata "longest" che ricerca la stringa più lunga all'interno del parametro ricevuto (un array di stringhe).
-*/
+ Scrivi una funzione chiamata "longest" che ricerca la stringa più lunga all'interno del parametro ricevuto (un array di stringhe).*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const longest = function (arr) {
+    let longestString = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].length > longestString.length) {
+            longestString = arr[i]
+        }
+    }
+    return longestString
+}
+
+console.log(longest(['strive', 'school', 'developer', 'student']))
 
 /* EXTRA 9
  Scrivi una funzione per creare un filtro anti spam molto semplice per una casella email. La funzione riceve una stringa come parametro, "emailContent", e ritorna un boolean.
  La funzione deve tornare true se emailContent NON contiene le parole "SPAM" o "SCAM".
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("Extra 9")
+
+const emailContent1 = "this is a scam";
+const emailContent2 = "this is a safe email";
+
+const filter = function (emailContent) {
+    const bannedWords = ["SPAM", "SCAM"]
+    for (word of bannedWords) {
+        if (emailContent.toLowerCase().includes(word.toLowerCase())) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(filter(emailContent1));
+console.log(filter(emailContent2));
 
 /* EXTRA 10
  Scrivi una funzione che riceve come parametro una data e ritorna il numero di giorni passati ad oggi.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const data = function (selectDate) {
+    let today = new Date()
+    let diffDate = today - selectDate
+    return Math.floor(diffDate / (1000 * 60 * 60 * 24))
+}
+console.log(data(new Date("2011-05-30")))
 
 /* EXTRA 11
  Scrivi una funzione chiamata "matrixGenerator" che riceve come parametri due interi, "x" e "y".
  La funzione deve tornare una matrice di x volte y, e ogni valore deve rappresentare l'indice dell'elemento.
  Es.: x = 3, y = 2
  ["00","01","02"
- "10","11","12"]
-*/
+ "10","11","12"]*/
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const matrixGenerator = function (x, y) {
+    let matrix = []
+    for (let i = 0; i < y; i++) {
+        for (let j = 0; j < x; j++) {
+            matrix.push(i.toString() + j.toString())
+
+        }
+    }
+    let carrPerRow = matrix.length / y
+    for (let z = 0; z < y; z++) {
+        console.log(matrix.slice(z * carrPerRow, (z + 1) * carrPerRow))
+    }
+}
+console.log(matrixGenerator(3, 4))
